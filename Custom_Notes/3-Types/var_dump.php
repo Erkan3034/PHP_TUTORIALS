f7c1831f728ee08619dd9a4d0314493c8fa87bd1
ERKAN TURGUT <159196857+Erkan3034@users.noreply.github.com>
2025-04-11 12:41:52 +0300

    Files added via upload.

---
+++ Custom_Notes/3-Types/var_dump.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$x = "Hello world!";
+$y = 'Hello world!';
+$z =455;
+var_dump($x);
+echo "<br>"; 
+var_dump($y);
+echo "<br>";
+gettype("Sonuc: ".$x);
+echo "<br>";
+var_dump($z);
+?>
+
+</body>
+</html>
