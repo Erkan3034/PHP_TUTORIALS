9fcbcc214a5cb723129921e64b63206c4ccf1cec
ERKAN TURGUT <159196857+Erkan3034@users.noreply.github.com>
2025-04-18 15:59:48 +0300

    Request and Get methods added!

---
+++ Custom_Notes/7- Form Islemleri/Request_Islemleri/request.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "<pre>";
+
+//print_r($_POST); // $_POST dizisini ekrana yazdırır. Bu dizi formdan gelen verileri tutar.
+
+//echo "<hr>";
+
+//print_r($_GET); // $_GET dizisini ekrana yazdırır. Bu dizi formdan gelen verileri tutar.
+echo "<hr>";
+print_r($_REQUEST); // $_REQUEST dizisini ekrana yazdırır. Bu dizi formdan gelen verileri tutar. $_POST ve $_GET dizilerini birleştirir.
+
+
+?>
