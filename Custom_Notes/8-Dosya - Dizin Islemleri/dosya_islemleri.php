2f4c76c594f6fea44d84eff5009e8da87e3d79b2
Erkan3034 <turguterkan55@gmail.com>
2025-05-03 23:13:06 +0300

    Updated dosya.txt and dosya_islemleri.php with additional content and improved file handling comments.

--- Custom_Notes/8-Dosya - Dizin Islemleri/dosya_islemleri.php
+++ Custom_Notes/8-Dosya - Dizin Islemleri/dosya_islemleri.php
@@ -9,9 +9,51 @@ touch("dosya.txt" , time()); // yeni dosya oluşturuldu ("time() ile oluşturulm
 unlink('test1.txt');
 
 
-//!======================== Dosya Açma===========================
-$dosya = fopen('dosya.txt','r'); // dosyayı aç.
+//!======================== Dosya Okuma===========================
+$dosya = fopen('dosya.txt','r'); // dosyayı aç
 
-echo fgets($dosya);  //dosya içeriğini getir.
+echo fgets($dosya);  //dosya içeriğini getir(ilk satırını getirir)
+echo "<hr>";
+
+/*
+while(!feof($dosya)){ // dosyanın sonuna kadar oku
+    echo fgets($dosya)."<br>";
+}
+
+*/
+
+//?======= fsize() ve fread() ile dosya okuma========
+
+$boyut = filesize('dosya.txt'); // dosyanın boyutunu al
+echo fread($dosya,$boyut); // dosyayı oku (ilk parametre dosya, ikinci parametre dosya boyutu)(boyut yerine istediğimiz miktarı yazabiliriz)
+echo "<hr>";
+
+fclose($dosya); // dosyayı kapat
+
+?>
+
+
+<?php
+
+/*
+============Dosya Erişim Modları - Dosya Kipleri====================
+
+r : Read işlemi. Dosyanın sadece içeriğinin okunması gerektiğini belirtir.
+
+r+ : Dosyanın içeriğinin he okunabilmesi hem de yazılabilmesinin gerektiğini belirtir.
+
+w : Dosyaya sadece yazma işleminin yapılabilmesinin gerektiğini belirtir. Dosyanın içeriği tamamen silinir ve baştan itibaren yazmaya başlanır. Dosya belirtilen adreste yok ise yeniden oluşturulur.
+
+w+ : Dosyaya hem yazma işleminin yapılabilmesi hem de dosya içeriğinin okunabilmesinin gerektiğini belirtir. Dosyanın içeriği silinir ve baştan veri yazma işlemi gerçekleştirilir. Dosya belirtilen adreste yok ise yeniden oluşturulur.
+
+a : Dosya içerisine sadece veri eklenebilmesi gerektiğini belirtir. Dosyanın içeriği silinmez, içeriğin sonuna veri eklenir. Dosya belirtilen adreste bulunmuyorsa yeniden oluşturulur.
+
+a+ : Dosyaya hem veri eklenebilmesini hem de verinin okunabilmesinin gerektiğini belirtir. Dosyanın içeriği silinmez, içeriğin sonuna veri eklenir. Dosya belirtilen adreste yok ise yeniden oluşturulur.
+
+x : Dosyanın oluşturulması sağlanır ve oluşturulan dosyanın içerisine veri yazmak için açılması gerektiğini belirtir. Aynı isimde bir dosya belirtilen adreste var ise fopen fonksiyonundan ‘false’ değeri döner.
+
+x+ : Dosyanın oluşturulması sağlanır ve içerisine hem veri yazma hem de verileri okunması gerektiğini belirtir. Dosya belirtilen adreste aynı isimde zaten var ise fopen fonksiyonundan ‘false’ değeri döner ve hata oluşur.
+
+*/
 
 ?>
