274f878bf6f14689254449e77d5184d5895581d5
Erkan3034 <turguterkan55@gmail.com>
2025-05-04 00:13:35 +0300

    Add file existence check and enhance documentation in dosya_islemleri.php

--- Custom_Notes/8-Dosya - Dizin Islemleri/dosya_islemleri.php
+++ Custom_Notes/8-Dosya - Dizin Islemleri/dosya_islemleri.php
@@ -58,22 +58,15 @@ echo "<hr>";
 fclose($dosya); // dosyayı kapat
 
 
+//?======================== Dosya varlığını kontrol etme===========================
 
+$control = file_exists('dosya.txt'); // dosyanın varlığını kontrol et (bool)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if($control){
+    echo "Dosya var";
+}else{
+    echo "Dosya yok";
+}
 ?>
 
 
@@ -96,6 +89,26 @@ fclose($dosya); // dosyayı kapat
 <?php
 
 /*
+is_file() : Dosyanın dosya olup olmadığını kontrol eder. Eğer dosya ise true, değilse false döner.
+is_dir() : Dosyanın dizin olup olmadığını kontrol eder. Eğer dizin ise true, değilse false döner.
+is_readable() : Dosyanın okunabilir olup olmadığını kontrol eder. Eğer okunabilir ise true, değilse false döner.
+is_writable() : Dosyanın yazılabilir olup olmadığını kontrol eder. Eğer yazılabilir ise true, değilse false döner.
+is_executable() : Dosyanın çalıştırılabilir olup olmadığını kontrol eder. Eğer çalıştırılabilir ise true, değilse false döner.
+is_uploaded_file() : Dosyanın yüklenip yüklenmediğini kontrol eder. Eğer yüklenmiş ise true, değilse false döner.
+
+file_exists() : Dosyanın var olup olmadığını kontrol eder. Eğer var ise true, değilse false döner.
+feof() : Dosyanın sonuna gelinip gelinmediğini kontrol eder. Eğer sona gelinmiş ise true, değilse false döner.
+fopen() : Dosyayı açar. İlk parametre dosya adı, ikinci parametre ise dosya erişim modudur. Eğer dosya açılamazsa false döner.
+fclose() : Dosyayı kapatır. İlk parametre dosya adıdır.
+fgets() : Dosyadan bir satır okur. İlk parametre dosya adı, ikinci parametre ise satır sayısıdır. Eğer dosya okunamazsa false döner.
+fread() : Dosyadan belirtilen boyutta veri okur. İlk parametre dosya adı, ikinci parametre ise okunacak boyuttur. Eğer dosya okunamazsa false döner.
+filesize() : Dosyanın boyutunu alır. İlk parametre dosya adıdır. Eğer dosya okunamazsa false döner.
+
+touch() : Dosyayı oluşturur. İlk parametre dosya adı, ikinci parametre ise oluşturulma tarihidir. Eğer dosya oluşturulamazsa false döner.
+unlink() : Dosyayı siler. İlk parametre dosya adıdır. Eğer dosya silinemezse false döner.
+
+
+
 ============Dosya Erişim Modları - Dosya Kipleri====================
 
 r : Read işlemi. Dosyanın sadece içeriğinin okunması gerektiğini belirtir.
