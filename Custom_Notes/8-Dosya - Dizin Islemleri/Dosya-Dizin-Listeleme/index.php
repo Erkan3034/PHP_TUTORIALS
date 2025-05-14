ca1e3ec3af7d7cebfccda77732c62dfe54a42e5c
Erkan3034 <turguterkan55@gmail.com>
2025-05-14 23:31:02 +0300

    Refactor index.php to list only .php files in the specified directory, enhancing output by removing the base directory path for better readability.

--- Custom_Notes/8-Dosya - Dizin Islemleri/Dosya-Dizin-Listeleme/index.php
+++ Custom_Notes/8-Dosya - Dizin Islemleri/Dosya-Dizin-Listeleme/index.php
@@ -14,10 +14,23 @@ print_r($array);
 echo "<hr>";
 
 // Kendi dizinin içindeki klasör/dosyaların tam yolunu  listeleme
-$array2  = glob($baseDir . '/*');
+$array2  = glob($baseDir . '/*.php');
 
 echo "<pre>";
 print_r($array2);
-echo "</pre>";
+echo "<hr>";
+
+
+// Kendi dizinin içindeki .php dosyalarını listeleme
+$array3 = glob($baseDir . '/*.php');
+
+//php dosyalarından önceki yolu kaldırarak dosyaları listeleme
+$dizi = array_map(function ($item)  use ($baseDir) {
+    return str_replace($baseDir, '' ,$item);
+}, $array3);
+
+echo "<pre>";
+print_r($dizi);
+
 
 ?>
